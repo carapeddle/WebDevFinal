af963f1e566552f75e4fbea8e7b671251ee4010e
Claire <xz4437@nyu.edu>
2024-12-11 19:47:19 -0500

    a

--- webdevFinal/search-history.php
+++ webdevFinal/search-history.php
@@ -9,13 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     }
 
     if ($query !== '') {
-        // Add new search term to history (prevent duplicates)
+        
         if (!in_array($query, $_SESSION['search_history'])) {
             $_SESSION['search_history'][] = $query;
         }
     }
 
-    // Filter history based on input
     $filtered_history = array_filter($_SESSION['search_history'], function ($term) use ($query) {
         return strpos($term, $query) !== false;
     });
